38c4f73314c0956a85df800ce7bda6265685b28a
jamilette04 <86180446+jamilette04@users.noreply.github.com>
2021-06-26 02:26:15 -0600

    Ejercicio 3
    Ejercicio 3- Jamilette Hernández

---
+++ Ejercicio 3 Arreglos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ejercicio_3_Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] datos = new int[20];
+            Console.WriteLine("Ingresa 20 numeros"); 
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Ingresa el numero " + (i + 1) + ": "); 
+                datos[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Clear(); string ans = " ";
+            Numeros numeros = new Numeros(datos); 
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"{datos[i]}  {numeros.Solucion()[i]}\n");
+            }
+        }
+    }
+}
+class Numeros
+{
+    int[] datos;
+    public Numeros(int[] datos)
+    {
+        Datos = datos;
+    }
+    public int[] Solucion()
+    {
+        Array.Sort(Datos);
+        return Datos;
+    }
+    public int[] Datos { get => datos; set => datos = value; }
+}  
