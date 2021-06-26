02cc3265e533efe7381118dd43a208736d0170c2
jamilette04 <86180446+jamilette04@users.noreply.github.com>
2021-06-26 02:27:00 -0600

    Ejercicio 4
    Ejercicio 4- Jamilette Hernández

---
+++ Ejercicio 4 Arreglos/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Ejercicio_4_Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("taller mecanico");
+            Console.ForegroundColor = ConsoleColor.White;
+            Taller taller = new Taller(5);
+            // se instancian los 5 numeros 
+            Console.WriteLine(taller.EspacioDisponible());
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Te mostramos los espacios disponibles");
+            Console.WriteLine("Al ingresar 'S' el programa se cerrara");
+            char ans; char salir = 'f';
+            while (salir != 'S')
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("¿Que es lo que desea hacer?");
+                Console.WriteLine("A)Meter en el taller \n");
+                Console.WriteLine("B)sacar del taller \n");
+                Console.WriteLine("Selecione una accion: ");
+                ans = char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Clear(); switch (ans)
+                {
+                    case 'A':
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine("Ha seleccionado meter en el taller ");
+                            Console.Write("Introduzca el numero de su placa: ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            string placa = Convert.ToString(Console.ReadLine());
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.Write("Introduzca su nombre porfavor : ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            string nombre = Convert.ToString(Console.ReadLine());
+                            if (taller.Meter(placa, nombre) == false)
+                            {
+                                Console.WriteLine("Lo sentimos, cupo llena");
+                                Console.ReadKey();
+                            }
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.Clear();
+                            Console.WriteLine("Se ha actualizado la cupo del taller ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine(taller.EspacioDisponible());
+                            Console.ReadKey(); break;
+                        }
+                    case 'B':
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine("Ha seleccionado sacar");
+                            if (taller.Sacar() == false)
+                            {
+                                Console.WriteLine("Lo sentimos, cupo vacio");
+                                Console.ReadKey();
+                            }
+                            Console.WriteLine("Se ha actualizado la tabla de cupo");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine(taller.EspacioDisponible());
+                            Console.ReadKey(); break;
+                        }
+                }
+                Console.Clear();
+                Console.WriteLine("Para salir seleccione la letra S o para continuar seleccione cualquier otra tecla");
+                salir = char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Ejercicio 4 Arreglos/Taller.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_4_Arreglos
+{
+    class Taller
+    {
+        //Atributos    
+        private string[,] carros;
+        private List<string> carrosAcumulados;
+        private List<int> monto;
+        private int indice;
+        private int indiceS;
+        //Constructor    
+        public Taller(int n)
+        {
+            //se ingresa el numero de la capacidad maxima de autos 
+            N = n;
+            Carros = new string[N, 2];
+            Monto = new List<int>();
+            CarrosAcumulados = new List<string>();
+            IndiceS = 0;
+            Indice = 0;
+            for (int rows = 0; rows < N; rows++)
+            {
+                for (int columns = 0; columns < 2; columns++)
+                {
+                    Carros[rows, columns] = " - ";
+                }
+            }
+        }
+        //metodo     
+        public Boolean Meter(string placas, string nombreDueño)
+        {
+            if (Indice < N)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    if (Carros[i, 0] == " - ")
+                    {
+                        Indice = i; break;
+                    }
+                }
+                Carros[Indice, 0] = placas;
+                Carros[Indice, 1] = nombreDueño; string value = $"{placas}, {nombreDueño}";
+                CarrosAcumulados.Add(value); return true;
+            }
+            return false;
+        }
+        public Boolean Sacar()
+        {
+            if (Indice - 1 >= 0) 
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    if (Carros[i, 0] != " - ")
+                    {
+                        Carros[i, 0] = Carros[i + 1, 0];
+                        Carros[i, 1] = Carros[i + 1, 1];
+                    }
+                }
+                Carros[N - 1, 0] = " - "; Carros[N - 1, 1] = " - "; return true;
+            }
+            return false;
+        }
+        public string EspacioDisponible()
+        {
+            String ans = "";
+            for (int rows = 0; rows < N; rows++)
+            {
+                ans += carros[rows, 0] + ", "; ans += carros[rows, 1] + "\n";
+            }
+            return ans;
+        }
+        public void Cobrar(int monto)
+        {
+            Monto.Add(monto);
+        }
+        public int[] GetMonto()
+        {
+            int[] AMonto = Monto.ToArray(); return AMonto;
+        }
+        public string[] GetCarrosAcumulados()    
+        {
+            string[] ACarrosAcumulados = CarrosAcumulados.ToArray(); return ACarrosAcumulados;
+        }
+        //propiedades    
+        public string[,] Carros
+        {
+            get => carros; set => carros = value;
+        }
+        public int N
+        {
+            get;
+        }
+        public List<int> Monto
+        {
+            get => monto; set => monto = value;
+        }
+        public int Indice
+        {
+            get => indice; set => indice = value;
+        }
+        public List<string> CarrosAcumulados
+        {
+            get => carrosAcumulados; set => carrosAcumulados = value;
+        }
+        public int IndiceS { get => indiceS; set => indiceS = value; }
+    }
+}
